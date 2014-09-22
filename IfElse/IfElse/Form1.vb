f4c96884cb46879e8f86c4e664027770435676c1
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:14:59 -0500

    work day in class

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -11,8 +11,20 @@ Public Class Form1
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
 
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Please type actual numbers.")
+            Return
+
+        End If
+
         num1 = CDbl(st1)
         num2 = CDbl(st2)
+
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Please type non-negative numbers.")
+            Return
+
+        End If
 
         Dim result As String
 
@@ -25,6 +37,8 @@ Public Class Form1
         End If
 
         txtResult.Text = result
+
+
     End Sub
 
 End Class
